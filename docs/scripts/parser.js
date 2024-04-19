b6c8fae749417c99b42a1de12d10f99f7249971e
halli-pronestor <halli@pronestor.com>
2024-04-19 13:11:55 +0000

    closetags

--- docs/scripts/parser.js
+++ docs/scripts/parser.js
@@ -1,10 +1,13 @@
 class _ParserRenderer {
-    static includeWrapperid = true;
+    static includeWrapperid = false;
     static Render(parsedResult, divToGet) {
         this.renderHtml(parsedResult, 0, divToGet);
+        console.log('fin');
+        this.CloseTags();
+    
     }
     static renderHtml(item, indent=0, parent){
-        var parent = this.AppendPre(item,indent+4, parent);
+        var parent = this.AppendPre(item, parent);
         if(item.children) {
             for(var ch of item.children){
                 this.renderHtml(ch, indent + 4, parent);
@@ -12,85 +15,118 @@ class _ParserRenderer {
         }
         else if(item.rest){
             if(parent !== 'body')
-                this.AppendPre(item.rest,indent, parent);
+                this.AppendPre(item.rest, parent);
         }
+        //this.CloseTags();
     }
     
-    static AppendPre(object, indent=0, parent){  
-        console.log(object, parent);
-        var parentEl = null;
+    static CloseTags(){
+        var elements = $('.element')
+        elements.each((i, el) => {
+            var j = $(el);
+            var text = j.html();
+            //debugger
+            text = text.replace('&lt;', '&lt;/').replace('&#60;', '&#60;/');            
+            //var htmlEncoded = _ParserHelpers.encodeHtml(text);
+            var domElement = _ParserHelpers.getElement('div', 'element', null, 'element');
+            domElement.append(text);
+            j.parent().parent().append(domElement);
+        });
+    }
+
+    static getParentElement(parent) {
+        if(!parent)
+            return $('body');
+            
+        var parentEl =  $(parent);
+        if(parentEl.length === 0){
+            debugger
+        }
+        return parentEl;        
+    }
+
+    static AppendHtml(parentEl, object, content) {
+        var domWrapper = _ParserHelpers.getElement('div', 'wrapper', null, 'wrapper');
+        var domDiv = _ParserHelpers.getElement('div', 'domdiv', null, 'domDiv');
+
+        if(content === '<body>')
+            content = '';
+            
+        if(object.start) {
+            var htmlEncoded = _ParserHelpers.encodeHtml(object.start);
+            var domElement = _ParserHelpers.getElement('div', 'element', null, 'element');
+            domElement.append(htmlEncoded);
+            domWrapper.html(domElement);
+        }
+        else
+            domWrapper.html(_ParserHelpers.encodeHtml(content));
+        if(this.includeWrapperid)
+            domWrapper.append(`<span class="sp">(${domDiv.attr('id')})</span>`)
+        domDiv.append(domWrapper);
+        parentEl.append(domDiv);
+        if(object.parsed){
+            var proppre = _ParserHelpers.getElement('div', 'proppre', null, 'proppre');
+            domDiv.append(proppre);
+            var parsedKeys = Object.keys(object.parsed);
+            
+            if(parsedKeys.length > 0){
+                for(var p of parsedKeys){
+                    var it = object.parsed[p];
+                    var prop = _ParserHelpers.getElement('div', null, null, 'prop');
+                    var jsonprop = '';
+                    if(it.indexOf('_json.') > -1){
+                        jsonprop = it;
+                        it = '';
+                    }
+                    prop.html(p+"="+it);
+                    if(jsonprop){
+                        var domDivLeaf = _ParserHelpers.getElement('span', 'jsontext', null, 'proppre');
+                        domDivLeaf.html(jsonprop);
+                        prop.append(domDivLeaf);
+                    }
+                    if(this.includeWrapperid)
+                        prop.append(`<span class="sp">(${prop.attr('id')})</span>`)
+                    proppre.append(prop);
+                }
+            }
+        }
+        return domDiv;
+    }
+
+    static AppendJson(parentEl, content) {      
+        this.AppendText(parentEl, JSON.stringify(content, null, 2));
+    }
+
+    static AppendText(parentEl, content) {
+        var domDivLeaf = _ParserHelpers.getElement('div', 'jsontext', null, 'proppre');
+        domDivLeaf.append('TEXT:' + content);
+        parentEl.append(domDivLeaf);
+    }
+    
+    static LastDomDiv = null;
+    static AppendPre(object, parent){
+        var parentEl = this.getParentElement(parent);
+
         var content = object;
         if(typeof object === 'object')        
             content = object.el;
             
-        var pre = _ParserHelpers.getElement('div', 'wrapper', null, 'wrapper');
-        if(!parent){
-            parentEl = $('body');
-        } else {
-            parentEl =  $(parent);
-            if(parentEl.length === 0){
-                debugger
-            }
-
-        }
         var domDiv = null;
         if(_ParserHelpers.StringIsHtml(content)){
-            if(content === '<body>')
-                content = '';
-            domDiv = _ParserHelpers.getElement('div', 'domDiv', null, 'domDiv');
-            if(indent > 0) {
-                domDiv.css({marginLeft: '40px'});
-            }
-            if(object.start)
-                pre.html(_ParserHelpers.encodeHtml(object.start));
-            else
-                pre.html(_ParserHelpers.encodeHtml(content));
-            if(this.includeWrapperid)
-                pre.append(`<span class="sp">(${domDiv.attr('id')})</span>`)
-            domDiv.append(pre);
-            parentEl.append(domDiv);
-            if(object.parsed){
-                var proppre = _ParserHelpers.getElement('div', 'proppre', null, 'proppre');
-                domDiv.append(proppre);
-                var parsedKeys = Object.keys(object.parsed);
-                
-                if(parsedKeys.length > 0){
-                    for(var p of parsedKeys){
-                        var it = object.parsed[p];
-                        var prop = _ParserHelpers.getElement('div', null, null, 'prop');
-                        var jsonprop = '';
-                        if(it.indexOf('_json.') > -1){
-                            jsonprop = it;
-                            it = '';
-                        }
-                        prop.html(p+"="+it);
-                        if(jsonprop){
-                            var domDivLeaf = _ParserHelpers.getElement('span', 'jsontext', null, 'proppre');
-                            domDivLeaf.html(jsonprop);
-                            prop.append(domDivLeaf);
-                        }
-                        if(this.includeWrapperid)
-                            prop.append(`<span class="sp">(${prop.attr('id')})</span>`)
-                        proppre.append(prop);
-                    }
-                }
-            }
+            domDiv = this.AppendHtml(parentEl, object, content);
+            this.LastDomDiv = domDiv;
+            return domDiv;
         }
         else if(_ParserHelpers.StringIsJson(content)){
-            pre.html(JSON.stringify(content, null, 2));
-            parentEl.append(pre);
+            this.AppendJson(parentEl, content);
         }
         else{  
-            var domDivLeaf = $('<span>')
-            var proppreId = 'proppre'+_ParserHelpers.getRandomInt(1, 1000);
-            domDivLeaf.attr('id', proppreId);
-            domDivLeaf.addClass('jsontext');
-            domDivLeaf.append(content);
-            parentEl.append(domDivLeaf);
+            console.log('this.LastDomDiv.children', this.LastDomDiv.children()[0]);
+            console.log('content', content);
+            this.AppendText($(this.LastDomDiv.children()[0]), content);
         }
-        return domDiv && domDiv.length > 0 ? '#'+ domDiv.attr('id') : '#'+ parentEl.attr('id'); // '#'+ pre.attr('id');
+        return parentEl;
     }
-
 }
 
 class _Parser {    
@@ -181,7 +217,7 @@ class _ParserHelpers {
     
     static StringIsHtml(str){
         if(!str){
-            debugger;
+            //debugger;
             return str;
         }
         return str.trim().indexOf('<') > -1;
@@ -194,7 +230,13 @@ class _ParserHelpers {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
     static getDomStart(t){
-        return t.split(' ')[0] + '>';
+        var start = t.split(' ')[0];
+        start = start + '>';
+        if(start.indexOf('>>') > -1){
+            start = start.split('>')[0];
+            start = start + '>';
+        }
+        return start;
     }
     static getAttributes ( $node ) {
         var attrs = {};
